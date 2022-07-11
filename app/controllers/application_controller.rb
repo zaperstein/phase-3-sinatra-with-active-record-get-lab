5a7e335f16ee17a8d91e72c5984dce81a97e34f6
Maggie Saperstein <maggie.saperstein@gmail.com>
2022-07-11 11:16:23 -0500

    commit tree 6c075a82ea5ab88f1252c9002a3b8de0dac81526

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,32 @@
 class ApplicationController < Sinatra::Base
 
+  #sets the Content-Type header in the response to application/json
+    set :default_content_type, 'application/json'
+
+    get '/bakeries' do
+      #get all the bakeries from the database
+     bakeries = Bakery.all
+      #send them back as a json array (array of objects)
+      bakeries.to_json
+    end
+
+    get '/bakeries/:id' do
+      #look up the bakery in the database using its ID
+      bakery = Bakery.find(params[:id])
+      #send a JSON-formatted response of the bakery data
+      bakery.to_json(include: :baked_goods)
+    end
+
+    get '/baked_goods/by_price' do
+    baked_goods = BakedGood.by_price
+    baked_goods.to_json
+    end
+
+    get '/baked_goods/most_expensive' do
+      baked_good = BakedGood.by_price.first
+      baked_good.to_json
+    end
+
   # add routes
 
 end
--- app/models/baked_good.rb
+++ app/models/baked_good.rb
@@ -1,3 +1,8 @@
 class BakedGood < ActiveRecord::Base
   # add association macro here
+  belongs_to :bakery
+
+  def self.by_price
+    self.all.order(:price).reverse
+  end
 end
